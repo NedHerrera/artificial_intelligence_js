b3d5c9efe89527f05c0c5280e7669e52b6ec90f9
Nery Eduardo Herrera <36248417+NedHerrera@users.noreply.github.com>
2021-12-06 22:01:17 -0600

    Update 01_reflex_agent.js
    first homework at 12/06/2021

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,5 +1,6 @@
 // MIT License
 // Copyright (c) 2020 Luis Espino
+// NED homework
 
 function reflex_agent(location, state){
    	if (state=="DIRTY") return "CLEAN";
@@ -8,18 +9,34 @@ function reflex_agent(location, state){
 }
 
 function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
-        	if (location == "A") states[1] = "CLEAN";
-         	else if (location == "B") states[2] = "CLEAN";
-      	}
-      	else if (action_result == "RIGHT") states[0] = "B";
-      	else if (action_result == "LEFT") states[0] = "A";		
-	setTimeout(function(){ test(states); }, 2000);
+       var location = states[0];		
+       var state = states[0] == "A" ? states[1] : states[2];
+       var action_result = reflex_agent(location, state);
+       document.getElementById("algoritmo").innerHTML+="<br> State: ".concat(states[1]).concat("/").concat(states[2]).concat("<br>Sector: ").concat(location).concat(" / Action: ").concat(action_result);	
+       if(states[1]=="CLEAN" && states[2]=="CLEAN")
+       {
+       		if(flag)
+		{
+            		document.getElementById("algoritmo").innerHTML+="<br>";
+            		return;
+        	}
+	       else
+	       { 
+            		flag = true;
+            		states[1] = "DIRTY";
+            		states[2] = "DIRTY";           
+        	}
+       	}
+	else if(action_result == "CLEAN")
+	{
+         	if (location == "A") states[1] = "CLEAN";
+          	else if (location == "B") states[2] = "CLEAN";
+       	}
+       	else if (action_result == "RIGTH") states[0] = "B";
+       	else if (action_result == "LEFT") states[0] = "A";	
+ 	setTimeout(function(){ test(states); }, 2000);
 }
 
-var states = ["A","DIRTY","DIRTY"];
+var  flag = false;
+var states = ["B","DIRTY","DIRTY"];
 test(states);
